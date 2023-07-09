259f25e11ee1ad891323fb585077d630c568714d
marcelo_oliveira <f.marcelo.qxda@gmail.com>
2023-07-09 19:59:21 -0300

    feat: add lib Phosphor

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,17 +1,34 @@
 import "../styles/sidebar.css";
+import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle } from "@phosphor-icons/react";
 
 function NavButton() {
   return (
     <>
       <div className="navButtonsContainer">
-        <a href="http://">Home</a>
-        <a href="http://">Explore</a>
-        <a href="http://">Notifications</a>
-        <a href="http://">Messages</a>
-        <a href="http://">Bookmarks</a>
-        <a href="http://">Lists</a>
-        <a href="http://">Profile</a>
-        <a href="http://">More</a>
+        <a className="active" href="">
+          <House weight="fill" /> Home
+        </a>
+        <a href="">
+          <Hash /> Explore
+        </a>
+        <a href="">
+          <Bell /> Notifications
+        </a>
+        <a href="">
+          <Envelope /> Messages
+        </a>
+        <a href="">
+          <BookmarkSimple /> Bookmarks
+        </a>
+        <a href="">
+          <FileText /> Lists
+        </a>
+        <a href="">
+          <User /> Profile
+        </a>
+        <a href="">
+          <DotsThreeCircle /> More
+        </a>
       </div>
     </>
   );
